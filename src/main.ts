7d50652ab8dd1943c96c5e98d731500d83a55cb9
Aicha Gaye <heliocentrique95@gmail.com>
2026-09-13 18:30:55 -0800

    secure backend cors origin

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+   app.enableCors({
+  origin: 'https://easyfact-hgxbwk5l1-moi-0aba.vercel.app',
+});
 
   app.useGlobalPipes(
     new ValidationPipe({

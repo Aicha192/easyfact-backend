afeac553f8908a5374a9a31ea288eb5e4d88077b
Aicha Gaye <heliocentrique95@gmail.com>
2026-09-14 19:51:20 -0800

    fix: allow production frontend cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
    app.enableCors({
-  origin: 'https://easyfact-5n3upommn-moi-0aba.vercel.app',
+     origin: [
+       'https://easyfact-chi.vercel.app',
+       'https://easyfact-5n3upommn-moi-0aba.vercel.app',
+  ],
 });
 
   app.useGlobalPipes(
